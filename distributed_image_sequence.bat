dcd555352ab2a898a937d96225abf75524ce7fa3
BryceAC <39605594+Bryce-AC@users.noreply.github.com>
2020-02-12 13:14:01 +1030

    Added initial files

---
+++ distributed_image_sequence.bat
@@ -0,0 +1,32 @@
+<# : Bryce's core roaster.bat
+@ECHO OFF
+color A
+echo Welcome to Bryce's core roaster
+echo It is time for your CPU to die
+echo Please select your After Effects project file:
+
+for /f "delims=" %%I in ('powershell -noprofile "iex (${%~f0} | out-string)"') do (
+    echo ... loading %%I
+	set "file=%%I"
+)
+
+set /p instances="How many instances of aerender.exe would you like? "
+
+:loop
+	if %instances% LEQ 0 goto exitloop
+	Start "Core roaster #%instances%" "C:\Program Files\Adobe\Adobe After Effects 2020\Support Files\aerender.exe" -project "%file%" -sound ON -v ERRORS_AND_PROGRESS
+	set /a instances=instances-1
+	goto loop
+:exitloop
+
+: end Batch portion / begin PowerShell hybrid chimera #>
+
+Add-Type -AssemblyName System.Windows.Forms
+$f = new-object Windows.Forms.OpenFileDialog
+$f.InitialDirectory = pwd
+
+$f.Filter = "AE Project Files (*.aep)|*.aep|All Files (*.*)|*.*"
+$f.ShowHelp = $true
+$f.Multiselect = $false
+[void]$f.ShowDialog()
+if ($f.Multiselect) { $f.FileNames } else { $f.FileName }
---
+++ multiple_file_output.bat
@@ -0,0 +1,41 @@
+::https://ss64.com/nt/syntax-args.html
+
+<# : Bryce's core roaster.bat
+@ECHO ON
+color A
+echo Welcome to Bryce's core roaster
+echo It is time for your CPU to die
+echo Please select your After Effects project file:
+
+for /f "delims=" %%I in ('powershell -noprofile "iex (${%~f0} | out-string)"') do (
+    echo ... loading %%I
+	set "file=%%I"
+)
+
+set /p frames="How many frames is your project? "
+set /p instances="How many instances of aerender.exe would you like? "
+set /A segment_length=%frames%/%instances%
+
+mkdir "render_output" 
+
+:loop
+	if %instances% LEQ 0 goto exitloop
+	set /a instances=instances-1
+	set /A start=%instances%*%segment_length%
+	set /A end=%start%+%segment_length%-1
+	Start "Core roaster #%instances%" "C:\Program Files\Adobe\Adobe After Effects CC 2019\Support Files\aerender.exe" -project "%file%" -s %start% -e %end% -comp "render" -rqindex 1 -sound ON -output "%~d0%~p0\render_output\out%instances%.mov"
+	goto loop
+:exitloop
+pause
+
+: end Batch portion / begin PowerShell hybrid chimera #>
+
+Add-Type -AssemblyName System.Windows.Forms
+$f = new-object Windows.Forms.OpenFileDialog
+$f.InitialDirectory = pwd
+
+$f.Filter = "AE Project Files (*.aep)|*.aep|All Files (*.*)|*.*"
+$f.ShowHelp = $true
+$f.Multiselect = $false
+[void]$f.ShowDialog()
+if ($f.Multiselect) { $f.FileNames } else { $f.FileName }
